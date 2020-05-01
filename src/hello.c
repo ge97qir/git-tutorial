ded33a53d434faf2d848408f12a3ff90c04fe834
Luka Keseric <ge97qir@mytum.de>
2020-05-02 00:51:58 +0200

    Implemented a patch to CMakeLists.txt, added a .patch file for the main function (part of the exercise) and completed the Coding Challenge 2.

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include "espl_lib.h"
+
+char repeat;
+unsigned int number;
+char* word;
 
 int main()
 {
-	printf("Hello ESPL!\n");
+	char repeat = 'y';
+	printf("Hello ESPL!");
 
+	while(repeat == 'y' || repeat == 'Y')
+	{
+		printf("\nPlease enter a number here: ");
+		scanf(" %u", &number);
+
+		word = num_to_words(number);
+		printf("You entered a number: %s", word);
+
+		printf("\nDo you want to enter another number? (y/n) ");
+		scanf(" %c", &repeat);
+	}
 	return 0;
 }
